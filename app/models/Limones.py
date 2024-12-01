6000aa549c696b48ebaa39a4c0d1f7e573c82b29
alemr214 <ale.mr0214@gmail.com>
2024-12-01 14:57:29 -0600

    Estableciendo los modelos de las tablas dentro de la base de datos

---
+++ app/models/Limones.py
@@ -0,0 +1,19 @@
+from app.database import Base
+from sqlalchemy import BigInteger, Column, ForeignKey, Numeric, String
+
+
+class Limones(Base):
+    __tablename__ = "limones"
+
+    id_limones = Column(BigInteger, primary_key=True, index=True, autoincrement=True)
+    color_cascara = Column(String(7))
+    textura_cascara = Column(String(30))
+    diametro = Column(Numeric(4, 2))
+    forma = Column(String(20))
+    brillo = Column(Numeric(5, 2))
+    uniformidad = Column(Numeric(5, 2))
+    peso = Column(Numeric(5, 2))
+    porosidad = Column(String(30))
+    nivel_madurez = Column(String(30))
+    certificacion = Column(BigInteger, ForeignKey("certificacion.no_etiquetado"))
+    id_pad = Column(BigInteger, ForeignKey("padecimiento.id_pad"))
---
+++ app/models/Padecimiento.py
@@ -0,0 +1,12 @@
+from app.database import Base
+from sqlalchemy import BigInteger, Column, Numeric, String
+
+
+class Padecimiento(Base):
+    __tablename__ = "padecimiento"
+
+    id_pad = Column(BigInteger, primary_key=True, index=True, autoincrement=True)
+    nombre_pad = Column(String(30))
+    concepto = Column(String(100))
+    tipo = Column(String(30))
+    area = Column(Numeric(5, 2))
