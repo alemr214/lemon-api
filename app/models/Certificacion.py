aee159b0d35030cde446062379ab9d166b47c90c
alemr214 <ale.mr0214@gmail.com>
2024-12-01 21:26:43 -0600

    Cambiando la ruta del script database en los archivos de modelo

--- app/models/Certificacion.py
+++ app/models/Certificacion.py
@@ -1,8 +1,9 @@
-from app.database import Base
+from database import Base
 from sqlalchemy import BigInteger, Column, String, Integer
 
 
 class Certificacion(Base):
+    __tablename__ = "certificacion"
     no_etiquetado = Column(BigInteger, primary_key=True, index=True, autoincrement=True)
     nombre_certificado = Column(String(50))
     nivel_calidad = Column(Integer)
--- app/models/Imagen_Limon.py
+++ app/models/Imagen_Limon.py
@@ -1,4 +1,4 @@
-from app.database import Base
+from database import Base
 from sqlalchemy import BigInteger, Column, ForeignKey, Integer, LargeBinary, Numeric
 
 
